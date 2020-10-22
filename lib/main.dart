e2477e54dd325b56b9c0b256cdc0d95bf7fc8680
subhendra-p <subhendra.rpandey@gmail.com>
2020-10-22 21:11:09 +0530

    added nav routes and click action in welcome screen

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,27 @@ import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
 
+import 'constants.dart';
+import 'constants.dart';
+import 'constants.dart';
+import 'constants.dart';
+import 'constants.dart';
+import 'constants.dart';
+import 'constants.dart';
+import 'constants.dart';
+import 'constants.dart';
+import 'constants.dart';
+import 'constants.dart';
+import 'screens/chat_screen.dart';
+import 'screens/chat_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/registration_screen.dart';
+import 'screens/registration_screen.dart';
+import 'screens/welcome_screen.dart';
+import 'screens/welcome_screen.dart';
+import 'screens/welcome_screen.dart';
+
 void main() => runApp(FlashChat());
 
 class FlashChat extends StatelessWidget {
@@ -12,10 +33,16 @@ class FlashChat extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData.dark().copyWith(
         textTheme: TextTheme(
-          body1: TextStyle(color: Colors.black54),
+          bodyText2: TextStyle(color: Colors.black54),
         ),
       ),
-      home: WelcomeScreen(),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id : (context) => WelcomeScreen(),
+        LoginScreen.id : (context) => LoginScreen(),
+        RegistrationScreen.id : (context) => RegistrationScreen(),
+        ChatScreen.id : (context) => ChatScreen()
+      },
     );
   }
 }
