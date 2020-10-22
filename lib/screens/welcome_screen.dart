868b079ae75f175e7829dfc1845c32434ce046e0
subhendra-p <subhendra.rpandey@gmail.com>
2020-10-22 22:03:48 +0530

    added ColorTween Animation

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -5,15 +5,43 @@ import 'registration_screen.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static const String id = 'welcome_screen';
+
   @override
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Animation animation;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+        duration: Duration(seconds: 2), vsync: this);
+    animation = ColorTween(
+      begin: Colors.blueGrey,
+      end: Colors.white
+    ).animate(_controller);
+    _controller.forward();
+    // _controller.reverse();
+    _controller.addListener(() {
+      setState(() {});
+      print(_controller.value);
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: animation.value,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
